c68006c180cccad44ef62ecaeb034f65161a9792
furkan eren <seferovic1905gmail.com>
2021-03-13 16:54:28 +0300

    Angular İlk Ders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'my site';
+  user: string = 'Engin Demirog';
+  
 }
+
